72a5f0c1a130076adc46fa1c1889cc20aacba084
Armeecoff <68rusdrug@gmail.com>
2026-05-12 19:52:03 +0300

    Add configuration loading from environment variables

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID", "")
+YOOKASSA_SECRET_KEY = os.getenv("YOOKASSA_SECRET_KEY", "")
+DB_PATH = os.getenv("DB_PATH", "bot.db")
